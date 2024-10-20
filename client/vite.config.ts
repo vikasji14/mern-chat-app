9dfee965c37829d7c981a5fc7eb0f0e67e785a2b
githubinfovikas <vikasdbg453@gmail.com>
2024-10-20 12:15:24 +0530

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://web-chat-app-rust.vercel.app/',
+//         // target: 'http://localhost:8000/',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://web-chat-app-rust.vercel.app/',
-        // target: 'http://localhost:8000/',
+      '/socket.io': {
+        target: 'https://web-chat-app-rust.vercel.app',
         changeOrigin: true,
+        ws: true,
       },
     },
   },
-})
+});
